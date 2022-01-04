be52523e9b9c24b94f1d999fb983a9aa7209b5ae
MaryahKim <maryapitaz@gmail.com>
2022-01-04 16:19:03 +0300

    Update app.js
    add picture url

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const app = Vue.createApp({
-    
-    data () {
-        return{
+
+    data() {
+        return {
             firstName: 'John',
             lastName: 'Doe',
             email: 'john@gmail.com',
             gender: 'male',
-            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg'
         }
     }
 })
